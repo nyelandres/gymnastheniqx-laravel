4bc802d7937aceb223b9ba6f4bf8d61df2d9e1a4
vincent <fabayjohnvincent@gmail.com>
2025-07-19 12:23:18 +0800

    i updated the routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserManagementController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SuppliersController;
+use App\Http\Controllers\InvoiceOutvoiceRecordsController;
+use App\Http\Controllers\ReportsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,7 +17,11 @@ Route::middleware('auth')->group(function () {
         return view('dashboard.index'); // ✅ FIXED HERE
     })->name('dashboard');
     Route::get('/user-management', [UserManagementController::class, 'index'])->name('user.management');
-});
+    Route::get('/products', [ProductsController::class, 'index'])->name('products');
+    Route::get('/suppliers', [SuppliersController::class, 'index'])->name('suppliers');
+    Route::get('/iorecords', [InvoiceOutvoiceRecordsController::class, 'index'])->name('invoice.outvoice.records');
+    Route::get('/reports', [ReportsController::class, 'index'])->name('reports');
+});     
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
